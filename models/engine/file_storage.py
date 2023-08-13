0dfe7e9111efc9a25f2450645af31eecadb76775
Bnobody47 <bnobodygetmoney@gmail.com>
2023-08-13 11:32:45 -0700

    Storage_Box

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,6 +2,12 @@
 import json
 from models.base_model import BaseModel
 import os
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 """
 Stores objects to file.
